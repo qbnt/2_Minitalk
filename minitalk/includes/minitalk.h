999ea18e4bcbe6ea97a213255241a3b10df6a667
qbnt <quentin.banet@hotmail.fr>
2023-04-12 13:52:38 +0200

    fsda

--- minitalk/includes/minitalk.h
+++ minitalk/includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:02:47 by qbanet            #+#    #+#             */
-/*   Updated: 2023/04/12 11:03:18 by qbanet           ###   ########.fr       */
+/*   Updated: 2023/04/12 11:57:39 by qbanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "libft.h"
 
-# define WAIT_TIME 100
+# define WAIT_TIME 50
 
 long long	ft_pow(long long nb, long long p);
 
--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,30 +6,33 @@
 /*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 14:30:22 by qbanet            #+#    #+#             */
-/*   Updated: 2023/04/12 11:14:46 by qbanet           ###   ########.fr       */
+/*   Updated: 2023/04/12 12:00:19 by qbanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int ft_error_in_args(int argc, char **argv)
+static int	ft_error_in_args(int argc, char **argv)
 {
-	int i;
+	int	i;
 
 	if (argc != 3)
 		return (-1);
 	i = -1;
 	while (argv[1][++i])
 	{
-		if (ft_strchr("1234567890", argv[1][i]))
+		if (!ft_strchr("1234567890", argv[1][i]))
+		{
+			ft_printf("Error\n");
 			return (1);
+		}
 	}
 	return (0);
 }
 
-static void ft_send_len(int len, int s_pid)
+static void	ft_send_len(int len, int s_pid)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (++i < 32)
@@ -43,9 +46,9 @@ static void ft_send_len(int len, int s_pid)
 	}
 }
 
-static void ft_send_next_char(unsigned char c, int s_pid)
+static void	ft_send_next_char(unsigned char c, int s_pid)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	while (++i < 8)
@@ -59,12 +62,12 @@ static void ft_send_next_char(unsigned char c, int s_pid)
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int s_pid;
-	char *str_to_send;
-	int len;
-	int i;
+	int		s_pid;
+	char	*str_to_send;
+	int		len;
+	int		i;
 
 	if (ft_error_in_args(argc, argv))
 		return (-1);
